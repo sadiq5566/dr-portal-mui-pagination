9433a961de56e3953db39154403ad2cbb9d6a1fb
randy <randy@govsoft.us>
2022-02-24 15:52:58 -0800

    integrate mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,4 @@
-import Login from "./components/Login/Login";
-import { BrowserRouter as Router, Route, Routes as Switch } from "react-router-dom";
-
 const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/login" element={<Login />} />
-      </Switch>
-    </Router>
-  );
+  return <>Enviornment setup</>;
 };
 export default App;
--- src/Pages/Login/Login.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Login from "../../components/Login/Login";
-const LoginOne = () => {
-  return <Login />;
-};
-
-export default LoginOne;
