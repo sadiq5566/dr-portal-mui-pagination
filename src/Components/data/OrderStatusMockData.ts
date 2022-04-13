85bd912144f5b67996e98f1fc642dfd4ea62af07
faraz <faraz@maxenius.agency>
2022-04-13 10:52:04 +0500

    order status changing

--- src/Components/data/OrderStatusMockData.ts
+++ src/Components/data/OrderStatusMockData.ts
@@ -2,20 +2,28 @@ const OrderStatusMockData = [
   {
     id: "1",
     name: "New Orders",
-    numbering: "41",
+    numbering: "82",
     location: "",
     orderStatus: true
   },
   {
     id: "2",
     name: "Unassigned Order",
-    numbering: "41",
+    numbering: "345",
     location: "",
 
     orderStatus: false
   },
   {
     id: "3",
+    name: "Batch Order",
+    numbering: "2345",
+    location: "",
+
+    orderStatus: false
+  },
+  {
+    id: "4",
     name: "All Orders",
     numbering: "41",
     location: "",
@@ -23,9 +31,9 @@ const OrderStatusMockData = [
     orderStatus: false
   },
   {
-    id: "4",
-    name: "Locked Orders",
-    numbering: "41",
+    id: "5",
+    name: "Pending Orders",
+    numbering: "45",
     location: "",
 
     orderStatus: false
